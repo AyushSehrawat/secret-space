5d086dfbffd5a9ea664c746ea71841ab5cc3915b
Ayush <scientificnationwords@gmail.com>
2022-11-26 16:39:14 +0530

    Plan changes | Very Much WIP

--- main.py
+++ main.py
@@ -1,56 +1,8 @@
 import os
-
-import arel
-from dotenv import load_dotenv
-
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, RedirectResponse, FileResponse
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-
 from routers import api as api_router
-
-load_dotenv()
-web_dev_mode = os.getenv("WEB_DEV_MODE", "False")
-
-if web_dev_mode == "True":
-    from starlette.applications import Starlette
-    from starlette.routing import WebSocketRoute
-
-    async def reload_data():
-        print("Reloading server data...")
-
-    hotreload = arel.HotReload(
-        paths=[
-            arel.Path("./templates", on_reload=[reload_data]),
-            arel.Path("./templates/static", on_reload=[reload_data])
-        ]
-    )
-
-    app = FastAPI(
-        routes=[WebSocketRoute("/hot-reload", hotreload, name="hot-reload")],
-        on_startup=[hotreload.startup],
-        on_shutdown=[hotreload.shutdown],
-    )
-
-    app.mount("/static", StaticFiles(directory="./templates/static"), name="static")
-    templates = Jinja2Templates(directory="./templates")
-
-    templates.env.globals["DEBUG"] = web_dev_mode
-    templates.env.globals["hotreload"] = hotreload
-
-else:
-    app = FastAPI()
-    app.mount("/static", StaticFiles(directory="./templates/static"), name="static")
-    templates = Jinja2Templates(directory="./templates")
-
-    templates.env.globals["DEBUG"] = web_dev_mode
-
-
-
-
-origins = ["https://localhost:5500", "https://localhost:8000"]
 
 app.add_middleware(
     CORSMiddleware,
@@ -62,11 +14,8 @@ app.add_middleware(
 
 @app.get("/")
 def index(request : Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return FileResponse(path="./templates/index.html")
 
-@app.get("/dashboard/{user_id}")
-def dashboard(request : Request, user_id : int):
-    return templates.TemplateResponse("dashboard.html", {"request": request, "user_id": user_id})
 
 app.include_router(api_router.router)
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,12 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "green-dark" : "#61892F",
-        "green-light" : "#86C232",
-        "gray-light" : "#6B6E70"
+        "green-dark" : "#61892F", 
+        "green-light" : "#86C232", 
+        "gray-light" : "#6B6E70",
+        "green-light" : "##34D399",
+        "cream-red" : "#FF7D7D",
+        "better-gray" : "#2E2E32"
       },
       fontFamily : {
         "primary" : "Rubik, sans-serif",
